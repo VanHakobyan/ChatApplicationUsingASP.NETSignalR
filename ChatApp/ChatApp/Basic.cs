df7598ec6641992e61addfc352e21a0169d00562
VanHakobyan <van19962013@mail.ru>
2017-05-04 18:16:32 +0400

    add basic:question: class:question:

---
+++ ChatApp/ChatApp/Basic.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatApp
+{
+    public partial class Basic
+    {
+        public void Config(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+        public class ChatHub : Hub
+        {
+            public void send(string name,string message)
+            {
+                Clients.All.broadcastMessage(name, message);
+            }
+        }
+    }
+}
